de97b31cff0c3e79a454053398ce5bd5aaea1482
jnevescruz <jnevescruz1@gmail.com>
2022-01-14 17:10:11 -0300

    type

--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,1 +1,26 @@
-console.log('TypeScript');
+//types
+//interfaces
+
+interface IAnimal {
+    nome: string
+    tipo: 'terrestre' | 'aquático'
+    domestico: boolean;
+}
+
+interface IFelino extends IAnimal{
+    visãoNoturna: boolean
+}
+
+interface ICanino extends IAnimal{
+    porte: 'pequeno' | 'medio' | 'grande'
+}
+
+type IDomestico = IFelino | ICanino /*type serve para fazer junçoes*/
+
+const animal: IDomestico = {
+    domestico: true,
+    nome: 'cachorro',
+    porte: 'medio',
+    tipo: 'terrestre',
+   }
+

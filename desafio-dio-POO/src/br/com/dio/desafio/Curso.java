abf08f26dd7692ea78aacb497d244d942c863b74
jnevescruz <jnevescruz1@gmail.com>
2022-03-22 15:55:36 -0300

    atualizacao 22/03

---
+++ desafio-dio-POO/src/br/com/dio/desafio/Curso.java
@@ -0,0 +1,9 @@
+package br.com.dio.desafio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+
+}
---
+++ desafio-dio-POO/src/br/com/dio/desafio/Mentoria.java
@@ -0,0 +1,7 @@
+package br.com.dio.desafio;
+
+public class Mentoria {
+    String titulo;
+    String descricao;
+    LocalDate data;
+}
